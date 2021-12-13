bbf1d92ae177f63604a674234515fc0301e70aa5
Dmitry <>
2021-12-13 18:01:31 +0200

    Created adding articles. These articles save in a database. We can read any article.

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-from flask import Flask
+from flask import Flask, request, redirect
 from flask import render_template  # Using for html templates
 from flask import url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
 
 
 @app.route('/')  # '@' is a decorator. In brackets path to a specific page on a website.
@@ -16,9 +32,41 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/user/<string:name>/<int:id>')  # <string:name> is name of a unique user.
-def user(name, id):
-    return "User page: " + name + " - " + str(id)
+@app.route('/posts')
+def posts():
+    articles = Article.query.order_by(Article.date.desc()).all()
+    return render_template("posts.html", articles=articles)
+
+
+@app.route('/posts/<int:id>')
+def post_detail(id):
+    article = Article.query.get(id)
+    return render_template("post_detail.html", article=article)
+
+
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article():
+    if request.method == 'POST':
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return "Something went wrong. Error."
+
+    else:
+        return render_template("create-article.html")
+
+
+# @app.route('/user/<string:name>/<int:id>')  # <string:name> is name of a unique user.
+# def user(name, id):
+#     return "User page: " + name + " - " + str(id)
 
 
 if __name__ == '__main__':
